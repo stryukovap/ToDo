533c2de68e21a8d1c0891f304c20ef79e5872fdd
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-09-27 17:51:17 +0300

    Hello world :)

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world!!!')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const el = <h1>Hello world</h1>
+console.log(el);
+// const el = React.createElement(
+//     'h1', null, 'Hello World!!!');
+
+ReactDOM.render(el,
+    document.getElementById('root')
+);
