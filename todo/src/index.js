ca8d002be1ed5b6d1de87782a71fec4eed3771fc
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-09-27 19:57:27 +0300

    same fiches

--- todo/src/index.js
+++ todo/src/index.js
@@ -14,16 +14,26 @@ const AppHeader = () => {
     return <h1>My Todo List</h1>;
 };
 const SearchPanel = () => {
-    return <input placeholder='search' />;
+    const searchText = 'Type here to search'
+    const searchStyle = {
+        fontSize:'20px'
+    };
+    return <input placeholder={searchText}
+    disabled={true} //default true!!!
+    className='foo' //class!!! for HTML
+    htmlFor='' 
+    style = {searchStyle}    />; //for!!! for label in HTML
 };
 const App = () => {
     const isLoggedIn = true; //change here for row 25
     const loginBox = <span>Log in please</span>;
     const welcomeBox = <span>Welcome Back</span>;
+    const value = '<script>alert("")</script>'
     return (
     <div>
     {isLoggedIn ? welcomeBox : loginBox}
     <p>{(new Date()).toString()}</p>
+    {value}
         <AppHeader />
         <SearchPanel />
         <TodoList />
