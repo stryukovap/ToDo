79393ea9177d1362b1392e176e7f1e9f621c83bd
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-09-27 18:19:46 +0300

    create React components

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,11 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = <h1>Hello world</h1>
-console.log(el);
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build Awesome App</li>
+        </ul>
+    );
+};
+const AppHeader = () => {
+    return <h1>My Todo List</h1>;
+};
+const SearchPanel = () => {
+    return <input placeholder='search' />;
+};
+const App = () => {
+    return (
+    <div>
+        <AppHeader />
+        <SearchPanel />
+        <TodoList />
+    </div>
+    );
+};
+// const el = <App />
+// const el = (
+//     <div>
+//         <AppHeader />
+//         <SearchPanel />
+//         <TodoList />
+//     </div>
+// )
+// const el = (
+//     <div>
+//         <h1>My Todo List</h1>
+//         <input placeholder='search' />
+//         <ul>
+//             <li>Learn React</li>
+//             <li>Build Awesome App</li>
+//         </ul>
+//     </div>
+// )
+// console.log(el);
 // const el = React.createElement(
 //     'h1', null, 'Hello World!!!');
 
-ReactDOM.render(el,
+ReactDOM.render(<App />,
     document.getElementById('root')
 );
