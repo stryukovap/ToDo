0f3745c460a072dcea995a61a99cf2bf311f0215
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-09-28 22:02:58 +0300

    refactored structure of project

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,66 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
-const TodoList = () => {
-    const items = ['Learn React', 'Build Awesome App']
-    return (
-        <ul>
-            <li>{items[0]}</li>
-            <li>{items[1]}</li>
-        </ul>
-    );
-};
-const AppHeader = () => {
-    return <h1>My Todo List</h1>;
-};
-const SearchPanel = () => {
-    const searchText = 'Type here to search'
-    const searchStyle = {
-        fontSize:'20px'
-    };
-    return <input placeholder={searchText}
-    disabled={true} //default true!!!
-    className='foo' //class!!! for HTML
-    htmlFor='' 
-    style = {searchStyle}    />; //for!!! for label in HTML
-};
 const App = () => {
-    const isLoggedIn = true; //change here for row 25
-    const loginBox = <span>Log in please</span>;
-    const welcomeBox = <span>Welcome Back</span>;
-    const value = '<script>alert("")</script>'
+
     return (
     <div>
-    {isLoggedIn ? welcomeBox : loginBox}
-    <p>{(new Date()).toString()}</p>
-    {value}
         <AppHeader />
         <SearchPanel />
         <TodoList />
     </div>
     );
 };
-// const el = <App />
-// const el = (
-//     <div>
-//         <AppHeader />
-//         <SearchPanel />
-//         <TodoList />
-//     </div>
-// )
-// const el = (
-//     <div>
-//         <h1>My Todo List</h1>
-//         <input placeholder='search' />
-//         <ul>
-//             <li>Learn React</li>
-//             <li>Build Awesome App</li>
-//         </ul>
-//     </div>
-// )
-// console.log(el);
-// const el = React.createElement(
-//     'h1', null, 'Hello World!!!');
 
 ReactDOM.render(<App />,
     document.getElementById('root')
