4b214734b6d416f65ca9c8e7a604f22259ab93cb
Andrey <31798016+stryukovap@users.noreply.github.com>
2018-09-28 22:31:33 +0300

    add array todo and visualise him

--- todo/src/components/todo-list.js
+++ todo/src/components/todo-list.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import TodoListItem from './todo-list-item';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
+
+    const elements = todos.map ((item) => {
+        return (
+            <li>
+                <TodoListItem {...item} />
+                {/*// label={item.label}*/}
+                {/*// important={item.important} />*/}
+            </li>
+        )
+    });
+
     return (
         <ul>
-            <li><TodoListItem label="Drink Coffee" /></li>
-            <li><TodoListItem label="Build React App" /></li>
+            { elements }
         </ul>
     );
 };
